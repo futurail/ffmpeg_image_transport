f5e064acb2102da9b59a8543f0d915b7a33ee99c
PatrickFuturail <francesco@futurail.com>
2023-10-08 09:24:39 +0200

    bind decoder

--- src/ffmpeg_decoder_pybind.cpp
+++ src/ffmpeg_decoder_pybind.cpp
@@ -1,55 +1,14 @@
 #include <pybind11/pybind11.h>
+
 #include "ffmpeg_image_transport/ffmpeg_decoder.hpp"
 
-#include <cv_bridge/cv_bridge.h>
-
-#include <fstream>
-#include <iomanip>
-#include <opencv2/imgcodecs.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <sensor_msgs/msg/image.hpp>
-#include <unordered_map>
-
-#define STRINGIFY(x) #x
-#define MACRO_STRINGIFY(x) STRINGIFY(x)
-
-
-
-int add(int i, int j) {
-    return i + j;
-}
-
 namespace py = pybind11;
+namespace ffmpeg_image_transport {
 
 PYBIND11_MODULE(ffmpeg_bindings, m) {
-    m.doc() = R"pbdoc(
-        Pybind11 example plugin
-        -----------------------
-
-        .. currentmodule:: cmake_example
-
-        .. autosummary::
-           :toctree: _generate
-
-           add
-           subtract
-    )pbdoc";
-
-    m.def("add", &add, R"pbdoc(
-        Add two numbers
-
-        Some other explanation about the add function.
-    )pbdoc");
-
-    m.def("subtract", [](int i, int j) { return i - j; }, R"pbdoc(
-        Subtract two numbers
-
-        Some other explanation about the subtract function.
-    )pbdoc");
-
-#ifdef VERSION_INFO
-    m.attr("__version__") = MACRO_STRINGIFY(VERSION_INFO);
-#else
-    m.attr("__version__") = "dev";
-#endif
+    py::class_<FFMPEGDecoder>(m, "FFMPEGDecoder")
+        .def(py::init<>()); 
+      
 }
+
+} // namespace ffmpeg_image_transport
