7f6a54d94c80ef6112d1fb32afad665ce7930be7
PatrickFuturail <francesco@futurail.com>
2023-10-10 15:17:16 +0200

    add python test

--- src/test.py
+++ src/test.py
@@ -13,6 +13,9 @@ rosbag_path = Path("/data/internal/infrabel_data/via_run_infrabel_2023-09-18_12-
 
 bag = BagFileParser(rosbag_path)
 
+
+msg = ffmpeg_bindings.cpp2py_img(10, 10)
+print(ffmpeg_bindings.cpp2py_ffmpeg(10, 10))
 topic = '/deg120/image'
 encoder.setParameters("libx264", "", "", "", 10, 8242880, 0, "")
 encoder.initialize(1920, 1080)
@@ -20,4 +23,8 @@ timestamps  = bag.get_timestamps(topic)
 for k, timestamp in enumerate(timestamps):
     
     msg = bag.get_message_for_timestamp(topic, timestamp)
-    print(encoder.encodeImage(msg))
+    print(type(msg.header))
+    
+    
+    out = encoder.encodeImage(msg)
+    print(type(out))
